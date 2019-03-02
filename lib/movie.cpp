e5b115dd4ddcc5338222552d0ad5f9ca72140b1b
Guy Dunton PC <guy.dunton@gmail.com>
2019-03-02 21:18:59 +0000

    Self encapsulate priceCode field

--- lib/movie.cpp
+++ lib/movie.cpp
@@ -1,9 +1,9 @@
 #include "movie.hpp"
 
 Movie::Movie(std::string title, int priceCode) :
-    title{ std::move(title) },
-    priceCode{ priceCode }
+    title{ std::move(title) }
 {
+	setPriceCode(priceCode);
 }
 
 const std::string& Movie::getTitle() const
